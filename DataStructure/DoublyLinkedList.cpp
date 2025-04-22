4c75d6801fdb5633ed2a46bd7e857cd7065c8e8b
Jaehyeok-Soh <revol3154@gmail.com>
2025-04-23 01:05:34 +0900

    done doublylinkedlist

--- DataStructure/DoublyLinkedList.cpp
+++ DataStructure/DoublyLinkedList.cpp
@@ -1,2 +1,125 @@
 #include "pch.h"
 #include "DoublyLinkedList.h"
+
+DoublyListNode::DoublyListNode()
+	: data(), prev(nullptr), next(nullptr){}
+DoublyListNode::DoublyListNode(int _data)
+	: data(_data), prev(nullptr), next(nullptr){}
+DoublyListNode::~DoublyListNode(){}
+
+DoublyList::DoublyList() 
+	: currentElementCount(0), pHead(nullptr), pTail(nullptr){}
+DoublyList::~DoublyList() { clearDoublyList(); }
+
+bool DoublyList::addDLElement(int position, int data)
+{
+	bool ret = false;
+
+	DoublyListNode* newNode(nullptr);
+	DoublyListNode* currentNode = pHead;
+	
+	if (position >= 0 && position <= currentElementCount)
+	{
+		newNode = new DoublyListNode(data);
+		if (!newNode)
+		{
+			std::cout << "error, allocate memory\n";
+			return ret;
+		}
+
+		for (auto i = 0; i < position; i++)
+		{
+			currentNode = currentNode->next;
+		}
+		newNode->prev = currentNode;
+		newNode->next = currentNode->next;
+		currentNode->next = newNode;
+		currentNode->next->prev = newNode;
+
+		currentElementCount++;
+		ret = true;
+	}
+	else
+	{
+		std::cout << "error, out of index\n";
+	}
+
+	return ret;
+}
+
+bool DoublyList::removeDLElement(int position)
+{
+	bool ret = false;
+	int arrayCount = currentElementCount;
+
+	if (position >= 0 && position < arrayCount)
+	{
+		DoublyListNode* tempNode(nullptr);
+		DoublyListNode* currentNode = pHead;
+
+		for (auto i = 0; i < position; i++)
+		{
+			currentNode = currentNode->next;
+		}
+		tempNode = currentNode->next;
+
+		currentNode->next = tempNode->next;
+		tempNode->next->prev = currentNode;
+		SAFE_DELETE(tempNode);
+
+		currentElementCount--;
+		ret = true;
+	}
+	else
+	{
+		std::cout << "error, out of index\n";
+	}
+
+	return ret;
+}
+
+void DoublyList::clearDoublyList()
+{
+	while (currentElementCount > 0)
+	{
+		removeDLElement(0);
+	}
+}
+
+DoublyListNode* DoublyList::getDLElement(int position)
+{
+	DoublyListNode* pReturn = nullptr;
+	DoublyListNode* currentNode = pHead;
+
+	if (position >= 0 && position < currentElementCount)
+	{
+		currentNode = pHead;
+		for (auto i = 0; i < position; i++)
+		{
+			currentNode = currentNode->next;
+		}
+		pReturn = currentNode;
+	}
+	else
+	{
+		std::cout << "error, out of index\n";
+	}
+
+	return pReturn;
+}
+
+void DoublyList::displayDoublyList()
+{
+	std::cout << "current element count : " << currentElementCount << std::endl;
+	if (currentElementCount > 0)
+	{
+		for (auto i = 0; i < currentElementCount; i++)
+		{
+			std::cout << "[" << i << "] : " << getDLElement(i)->data << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "list is empty\n";
+	}
+}
--- DataStructure/DoublyLinkedList.h
+++ DataStructure/DoublyLinkedList.h
@@ -1,4 +1,32 @@
 #pragma once
-class DoublyLinkedList
+class DoublyListNode
 {
+public:
+	int data;
+	DoublyListNode* prev;
+	DoublyListNode* next;
+
+	DoublyListNode();
+	DoublyListNode(int _data);
+	~DoublyListNode();
+};
+
+class DoublyList
+{
+private:
+	int currentElementCount;
+
+public:
+	DoublyList();
+	~DoublyList();
+
+	DoublyListNode* pHead;
+	DoublyListNode* pTail;
+
+	void deleteDoublyList();
+	bool addDLElement(int position, int data);
+	bool removeDLElement(int position);
+	void clearDoublyList();
+	DoublyListNode* getDLElement(int position);
+	void displayDoublyList();
 };
